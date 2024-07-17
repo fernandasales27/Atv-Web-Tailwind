f8b76f53bdc818e0db7bba2d9b28163bb9ef8659
fernandasales27 <eifernandasales27@gmail.com>
2024-07-17 18:40:32 -0300

    commit certo 2

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  purge: ['./*.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
